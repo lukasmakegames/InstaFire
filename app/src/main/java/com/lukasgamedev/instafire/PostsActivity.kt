a5ceb6049ce3031940fc856a3e414dcadc06d1d0
lukasgamedev <lugiro43@gmail.com>
2022-09-30 13:07:23 +0200

    request posts from firebase

--- app/src/main/java/com/lukasgamedev/instafire/PostsActivity.kt
+++ app/src/main/java/com/lukasgamedev/instafire/PostsActivity.kt
@@ -3,13 +3,35 @@ package com.lukasgamedev.instafire
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreException
+
+private const val TAG = "PostActivity"
 
 class PostsActivity : AppCompatActivity() {
+
+    private lateinit var firestoreDb: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_posts)
+
+        firestoreDb = FirebaseFirestore.getInstance()
+        val postsReference = firestoreDb.collection("posts")
+        postsReference.addSnapshotListener {
+            snapshot, exception ->
+            if (exception != null || snapshot == null){
+                Log.e(TAG,"Esception when quering post", exception)
+                return@addSnapshotListener
+            }
+
+            for (document in snapshot.documents){
+                Log.i(TAG,"Document ${document.id}: ${document.data}")
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
